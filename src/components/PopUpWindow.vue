5dd82fa62f3610b56345938d38c3a21fa2577db8
vadim <vadim.lylko@gmail.com>
2024-10-13 23:50:36 +0300

    create an empty PopUpWindow.vue

---
+++ src/components/PopUpWindow.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="pop-up">
+
+</div>
+</template>
+
+<style scoped lang="scss">
+@import "src/assets/styles/variables";
+.pop-up {
+
+}
+</style>
