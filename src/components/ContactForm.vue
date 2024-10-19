bce7b3cce0bfe979f52291b93372902efa1e0409
vadim <vadim.lylko@gmail.com>
2024-10-19 18:41:01 +0300

    layout form for ContactForm.vue

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -38,6 +38,27 @@ const props = defineProps({
       </li>
     </ul>
   </div>
+  <div class="contacts__bottom contacts-bottom">
+    <h3 class="contacts-bottom__title">Зворотній зв’язок</h3>
+    <form action="#" class="contacts-bottom__form">
+      <div class="form-control">
+        <label for="user-name">Прізвище Ім'я</label>
+        <input name="user-name" type="text">
+      </div>
+
+      <div class="form-control">
+        <label for="user-phone">Телефон</label>
+        <input name="user-phone" type="number">
+      </div>
+
+      <div class="form-control">
+        <label for="user-comment">Коментар</label>
+        <textarea name="user-comment" type="text" rows="3"></textarea>
+      </div>
+
+      <button class="btn-reset form__btn">Відправити</button>
+    </form>
+  </div>
 </div>
 </template>
 
@@ -69,6 +90,59 @@ const props = defineProps({
         &:hover{
           color: #009eff;
         }
+      }
+    }
+  }
+  &-bottom{
+    margin-top: 20px;
+    &__title{
+      font-size: 17px;
+      margin-bottom: 10px;
+    }
+    &__form{
+      display: flex;
+      flex-direction: column;
+      .form-control{
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 12px;
+      }
+      label{
+        margin-bottom: 5px;
+        font-size: 14px;
+        color: #939398;
+      }
+      input, textarea{
+        width: 100%;
+        outline: none;
+        border: none;
+        background-color: #efefef;
+        color: inherit;
+        letter-spacing: 0.01em;
+        font-size: 16px;
+        resize: none;
+        padding: 14px 15px;
+        border-radius: $card-border-radius;
+      }
+      input[type="number"]::-webkit-outer-spin-button,
+      input[type="number"]::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+      }
+
+      input[type="number"] {
+        -moz-appearance: textfield;
+      }
+      button{
+        font-weight: 500;
+        border-radius: $card-border-radius;
+        background-color: $orange-color;
+        padding: 14px 15px;
+        color: $text-color-light;
+      }
+      .form__btn{
+        width: 100%;
+        margin: 12px auto 8px;
       }
     }
   }
